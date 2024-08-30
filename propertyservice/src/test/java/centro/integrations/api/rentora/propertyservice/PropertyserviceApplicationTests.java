5851917411fc860a09e6bf8a0a979075da87207d
usrinivasanu <USRINIVASANU@GMAIL.COM>
2024-08-29 21:14:45 -0500

    Initial commit master

---
+++ propertyservice/src/test/java/centro/integrations/api/rentora/propertyservice/PropertyserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package centro.integrations.api.rentora.propertyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
